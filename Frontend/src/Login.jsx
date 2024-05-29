174b849a01e31b7b8cc5714417b0d90367716453
Luis Villa <vluis@unc.edu>
2024-05-29 00:26:26 -0400

    Add flask dependencies and test file, replaced frontend with Vite+React

---
+++ Frontend/src/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import './Login.css'
+
+
+const Login = () => {
+
+   const [action,setAction] = useState("Sign Up");
+
+ return (
+
+   <div className='container'>
+       <div className="header">
+            <div className="text">ChapelEats</div>
+           <div className="text">{action}</div>
+       </div>
+           <div className="inputs">
+               {action==="Login"?<div></div>:<div className="input">
+                   <input type='name' placeholder='Name'/>
+               </div>}
+               <div className="input">
+                   <input type='email'placeholder='Email'/>
+               </div>
+               <div className="input">
+                   <input type='password' placeholder='Password'/>
+               </div>
+           </div>
+
+
+       <div className="submit-container">
+           <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+           <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+
+
+       </div>
+   </div>
+ )
+}
+
+
+export default Login
