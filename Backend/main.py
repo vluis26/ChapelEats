fb28a9b3599b1b6fee57f541873792b609e10ea0
Luis Villa <vluis@unc.edu>
2024-06-18 05:35:57 -0400

    deployment part12

--- Backend/main.py
+++ Backend/main.py
@@ -202,5 +202,5 @@ def get_saved_meals():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
